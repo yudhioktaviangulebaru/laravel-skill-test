23946e78cc8c5402487d5ed890ac989b28a9d0c6
yudhioktaviangulebaru <yudhioktaviangule@gmail.com>
2026-01-13 08:21:09 +0700

    Flattens post route definitions
    Removes the grouped route definition for post-related routes. Each route now explicitly defines its middleware and controller, improving clarity and making the full configuration of each route self-contained.

--- routes/posts.php
+++ routes/posts.php
@@ -4,16 +4,33 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('posts', [PostController::class, 'index'])
+    ->name('posts.index');
 
-Route::middleware('auth')
-    ->prefix('/posts')
-    ->controller(PostController::class)
-    ->group(function () {
-        Route::post('/', 'store')->name('posts.store')->can('create', Post::class);
-        Route::get('/create', 'create')->name('posts.create')->can('create', Post::class);
-        Route::get('/{post}/edit', 'edit')->name('posts.edit')->can('update', 'post');
-        Route::match(['put', 'patch'], '/{post}', 'update')->name('posts.update')->can('update', 'post');
-        Route::delete('/{post}', 'destroy')->name('posts.destroy')->can('delete', 'post');
-    });
+Route::post('posts', [PostController::class, 'store'])
+    ->name('posts.store')
+    ->middleware('auth')
+    ->can('create', Post::class);
+
+Route::get('posts/create', [PostController::class, 'create'])
+    ->name('posts.create')
+    ->middleware('auth')
+    ->can('create', Post::class);
+
+Route::get('posts/{post}', [PostController::class, 'show'])
+    ->name('posts.show');
+
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])
+    ->name('posts.edit')
+    ->middleware('auth')
+    ->can('update', 'post');
+
+Route::match(['put', 'patch'], 'posts/{post}', [PostController::class, 'update'])
+    ->name('posts.update')
+    ->middleware('auth')
+    ->can('update', 'post');
+
+Route::delete('posts/{post}', [PostController::class, 'destroy'])
+    ->name('posts.destroy')
+    ->middleware('auth')
+    ->can('delete', 'post');
